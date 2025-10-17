264ce4a58973cb813676e71f882cc6a187daf601
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-10-17 16:40:56 -0300

    Variaveis Dart

---
+++ variaveis.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+  // Produtos
+  
+  int codigo = 3;
+  double preco = 3.14;
+  String nome = 'Pendrive';
+  bool venda = true;
+  var variavel = 1;
+  dynamic variavel2 = 1;
+  variavel2 = 'Lucas';
+  
+  print(codigo);
+  print(preco);
+  print(nome);
+  print(venda);
+  print(variavel);
+  print(variavel2);
+  
+}
