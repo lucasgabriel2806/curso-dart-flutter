93784ba66ef2e87c16b3ee506476e793e4fa7c08
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-10-18 11:36:04 -0300

    Incremento e Decremento

---
+++ incremento_decremento.dart
@@ -0,0 +1,18 @@
+void main() {
+  
+   int numero = 10;
+  
+  // numero = numero + 1
+  
+  // numero += 1
+  // numero -= 1
+  
+  // numero++
+  // numero--
+  
+  // ++numero
+  // --numero
+  
+  print(numero);
+   
+}
