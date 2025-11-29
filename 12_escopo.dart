80fb13d8415d615949249a42bf549c70f3eb8072
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-11-28 23:48:52 -0300

    Adicionar escopo

---
+++ 12_escopo.dart
@@ -0,0 +1,34 @@
+int resultado = 100;
+
+// const PI = 3.14;
+
+void main() {
+  
+  /**
+   * 
+   * int resultado = 50;
+   * 
+   * print(soma(10, 20));
+   * 
+   * print(resultado);
+   * 
+   */
+
+  int i = 100;
+
+  for (int i = 0; i < 10; i++) {
+
+    print(i);
+
+  }
+
+  print(i);
+}
+
+int soma(int numero1, int numero2) {
+
+  int resultado = numero1 + numero2;
+
+  return resultado;
+
+}
