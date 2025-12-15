acc5409aa1cff2ea154cfb8c7ec95495761244b5
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-12-15 15:45:29 -0300

    Adicionar const-final

---
+++ 18_const-final.dart
@@ -0,0 +1,16 @@
+void main() {
+  // const int a = 2;
+  // final int a = 2;
+
+  // const é para coisas que são conhecidas/fixas 
+  // quando você está executando seu programa,
+  // por isso gera erro.
+  // const DateTime data = new DateTime.now();
+
+  // final é para coisas que você pode obter de fora
+  final DateTime data = new DateTime.now();
+
+  print(new DateTime.now());
+
+  // print(a);
+}
