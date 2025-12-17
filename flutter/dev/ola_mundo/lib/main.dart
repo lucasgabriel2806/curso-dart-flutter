e5afb288c8e5c84586770335a1b4d63425689888
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-12-17 15:05:30 -0300

    Adicionar app flutter ola_mundo

---
+++ flutter/dev/ola_mundo/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  //runApp(const MyApp());
+
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text(
+          'olá mundo', 
+          style: TextStyle(fontSize: 50, color: Colors.white)
+        ),
+      ),
+    ),
+  );
+}
